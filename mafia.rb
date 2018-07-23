157a28613542dc4c935cfd76cd128dd41a70eb2c
sgudur9 <18sgudur.stem@gmail.com>
2018-07-23 19:56:15 +0000

    automatically backed up by learn

--- mafia.rb
+++ mafia.rb
@@ -4,3 +4,4 @@ puts "Detective, who would you like to investigate?"
 suspect = gets.chomp
 puts "Doctor, who would you like to save?"
 patient = gets.chomp
+puts "In Killadelphia, the mafia killed #{victim}, detective thought it was #{suspect} while the doctor saved #{patient}"
